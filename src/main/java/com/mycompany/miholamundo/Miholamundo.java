7b1b4f41aa4a819e6c6e1b497ec8f0e838c35772
Alejastrone <alejastrone@gmail.com>
2023-03-12 13:31:14 -0300

    Hola Mundo

---
+++ src/main/java/com/mycompany/miholamundo/Miholamundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miholamundo;
+
+/**
+ *
+ * @author astr0b1te
+ */
+public class Miholamundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo te voy a comer como el Dibu XD!");
+    }
+}
